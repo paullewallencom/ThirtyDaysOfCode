48817ab7a3a3218b8dfb2ca2fd18f1dc6bcd1cfc
Paul Lewallen <paul.lewallen@gmail.com>
2018-01-14 14:33:09 -0800

    Added properties to type car.

--- ThirtyDaysOfCode/src/day01OfCode/Car.java
+++ ThirtyDaysOfCode/src/day01OfCode/Car.java
@@ -15,10 +15,17 @@
      * @param args the command line arguments
      *
      */
+	  
+	 int maxSpeed = 100;
+	 int minSpeed = 0;
+	 double weight = 4079;
+	 boolean isTheCarOn = false;
+	 char condition = 'A';
      
      public static void main(String[] args)
      {
-        //TODO code application logic here
-    	 	System.out.println("Hello Car!"); 
+    	 	//TODO code application logic here
+    	 	System.out.println("Hello Car!");
+
      }
   }
