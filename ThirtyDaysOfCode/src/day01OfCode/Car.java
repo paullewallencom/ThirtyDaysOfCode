d6779333b112b0b610b615d4ccc8e113eca9a3f2
Paul Lewallen <paul.lewallen@gmail.com>
2018-01-14 14:38:42 -0800

    Instantiate instance of class type Car.

--- ThirtyDaysOfCode/src/day01OfCode/Car.java
+++ ThirtyDaysOfCode/src/day01OfCode/Car.java
@@ -21,11 +21,20 @@
 	 double weight = 4079;
 	 boolean isTheCarOn = false;
 	 char condition = 'A';
+	 
+	 public void printVariables()
+	 {
+		 System.out.println(maxSpeed);
+		 System.out.println(minSpeed);
+		 System.out.println(weight);
+		 System.out.println(isTheCarOn);
+		 System.out.println(condition);
+	 }
      
      public static void main(String[] args)
      {
-    	 	//TODO code application logic here
-    	 	System.out.println("Hello Car!");
-
+    	 	//TODO code application logic here    	 	System.out.println("Hello Car!");
+    	 	Car myCar = new Car();
+    	 	myCar.printVariables();
      }
   }
