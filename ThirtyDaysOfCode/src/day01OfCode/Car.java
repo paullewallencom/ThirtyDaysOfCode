03f0ca8b136066f077c2163bef8b365ecca9118e
Paul Lewallen <paul.lewallen@gmail.com>
2018-01-14 14:22:25 -0800

    Create Car class.

---
+++ ThirtyDaysOfCode/src/day01OfCode/Car.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2018 Paul Lewallen
+ *
+ */
+ package day01OfCode;
+ 
+ /**
+  * Author paullewallen
+  *
+  */
+  public class Car
+  {
+    
+    /**
+     * @param args the command line arguments
+     *
+     */
+     
+     public static void main(String[] args)
+     {
+        //TODO code application logic here
+    	 	System.out.println("Hello Car!"); 
+     }
+  }
