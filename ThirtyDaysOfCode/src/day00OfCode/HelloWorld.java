8417e07a6b225c499cbd615e92a25d5a9e158853
Paul Lewallen <paul.lewallen@gmail.com>
2018-01-14 12:45:31 -0800

    Init

---
+++ ThirtyDaysOfCode/src/day00OfCode/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2018 Paul Lewallen
+ *
+ */
+ package day00OfCode;
+ 
+ /**
+  * Author paullewallen
+  *
+  */
+  public class HelloWorld
+  {
+    
+    /**
+     * @param args the command line arguments
+     *
+     */
+     
+     public static void main(String[] args)
+     {
+        //TODO code application logic here
+    	 	System.out.println("Hello World!");
+     }
+  }
