083ebf6858976185c2ff9b7d3e90d6b3e0b4605e
Paul Lewallen <paul.lewallen@gmail.com>
2018-01-14 20:12:56 -0800

    Init

---
+++ ThirtyDaysOfCode/src/day02OfCode/Car.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2018 Paul Lewallen
+ *
+ */
+ package day02OfCode;
+
+ /**
+  * Author paullewallen
+  *
+  */
+  public class Car
+  {
+    
+    /**
+     * @param args the command line arguments
+     *
+     */
+	  
+	 int maxSpeed = 100;
+	 int minSpeed = 0;
+	 
+	 double weight = 4079;
+	 
+	 boolean isTheCarOn = false;
+	 
+	 char condition = 'A';
+	 
+	 String nameOfCar = "Pablo Picante";
+	 
+	 public void printVariables()
+	 {
+		 System.out.println("This is the maxSpeed " + maxSpeed);
+		 System.out.println(minSpeed);
+		 System.out.println(weight);
+		 System.out.println(isTheCarOn);
+		 System.out.println(condition);
+		 System.out.println(nameOfCar);
+	 }
+	 
+	 public void upgradeMinSpeed()
+	 {
+		 minSpeed = maxSpeed;
+		 maxSpeed = maxSpeed + 50;
+	 }
+	 
+	 public static void main(String[] args)
+     {	
+    	 	// TODO code application logic here
+		 Car familyCar = new Car();
+		 System.out.println("Family's Car:");
+		 familyCar.printVariables();
+		 familyCar.upgradeMinSpeed();
+		 
+     }
+  }
